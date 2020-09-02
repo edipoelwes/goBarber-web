565ca432b8953beb7d7355b205f55c3f9ce02358
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-09-02 12:06:11 -0300

    Criando a estrutura de Toast

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -46,8 +46,12 @@ const SingIn: React.FC = () => {
           password: data.password,
         })
       } catch (err) {
-        const errors = getValidationErrors(err)
-        formRef.current?.setErrors(errors)
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+          formRef.current?.setErrors(errors)
+        }
+
+        // disparar um Toast
       }
     },
     [signIn],
