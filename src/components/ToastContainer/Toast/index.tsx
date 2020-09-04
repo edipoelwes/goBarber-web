4592307e337e85d34063d6fcd64ebc06b598cdfc
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-09-04 16:04:39 -0300

    Animando Toasts

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -6,6 +6,7 @@ import { FiAlertCircle, FiXCircle, FiCheckCircle, FiInfo } from 'react-icons/fi'
 
 interface ToastProps {
   message: ToastMessage
+  style: object
 }
 
 const icons = {
@@ -14,7 +15,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 }
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast()
 
   useEffect(() => {
@@ -27,7 +28,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
     }
   }, [removeToast, message.id])
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
