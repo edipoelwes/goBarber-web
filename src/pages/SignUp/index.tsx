21a0d755118f4a9936f6131f12caaf7a3c738f53
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-09-04 16:52:53 -0300

    Configurando rotas e animaçao nas rotas

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useRef } from 'react'
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
+import { Link } from 'react-router-dom'
 import * as Yup from 'yup'
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -10,7 +11,7 @@ import logoImg from '../../assets/images/logo.svg'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, Background, AnimationContainer } from './styles'
 
 interface RegisterProps {
   name: string
@@ -45,27 +46,29 @@ const SignUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
 
-          <Button type="submit">Cadastrar</Button>
-        </Form>
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <a href="teste">
-          <FiArrowLeft />
-          Voltar para logon
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   )
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { shade } from 'polished'
 
 import signupBackgroundImg from '../../assets/images/sign-up-background.png'
@@ -13,11 +13,29 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  place-content: center;
-
+  justify-content: center;
   width: 100%;
   max-width: 700px;
+`
+
+const appearFromRigth = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px)
+  }
+
+  to{
+    opacity: 1;
+    transform: translateX(0)
+  }
+`
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRigth} 1s;
 
   form {
     margin: 80px 0;
@@ -42,7 +60,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -54,11 +72,10 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `
-
 export const Background = styled.div`
   flex: 1;
   background: url(${signupBackgroundImg}) no-repeat center;
